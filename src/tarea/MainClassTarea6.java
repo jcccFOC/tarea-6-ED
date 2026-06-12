31a4befd4fa25092968d35ecd3ccd117a6c35fc5
JcccFOC <mailjust4spam@gmail.com>
2026-06-13 01:07:17 +0200

    Primer commit del proyecto de la tarea6

---
+++ src/tarea/MainClassTarea6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author Jccc
+ */
+public class MainClassTarea6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+        miCarrito.aplicarDescuento(100, 5);
+    }
+
+}
---
+++ src/tarea/Tarea6EDClase.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author Jccc
+ */
+public class Tarea6EDClase {
+
+    public void aplicarDescuento(double precioProducto, int numProductos) {
+        if (numProductos > NUM_PRODUCTOS_PARA_DESCUENTO_FIJO) {
+            precioProducto -= DESCUENTO_FIJO;
+        }
+        if (numProductos != NUM_PRODUCTOS_PARA_DESCUENTO_PORCENTAJE_EXTRA) {
+            double descuentoPorcentaje = 0.8;
+            calculoPagoYEnvio(precioProducto, descuentoPorcentaje);
+        } else {
+            double descuentoPorcentaje = 0.95;
+            calculoPagoYEnvio(precioProducto, descuentoPorcentaje);
+        }
+
+    }
+
+    private void calculoPagoYEnvio(double precioProducto, double descuentoPorcentaje) {
+        double totalPagar = precioProducto * descuentoPorcentaje;
+        System.out.println("El total a pagar es:" + totalPagar);
+        System.out.println("Enviado");
+    }
+    
+    private static final double DESCUENTO_FIJO = 5;
+    private static final int NUM_PRODUCTOS_PARA_DESCUENTO_FIJO = 3;
+    private static final int NUM_PRODUCTOS_PARA_DESCUENTO_PORCENTAJE_EXTRA = 0;
+}
